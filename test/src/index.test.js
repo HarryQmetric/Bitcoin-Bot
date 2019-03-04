0da7ab5d2e73f6a383df299f1a472b9ecee8703e
HarryQmetric <46711154+HarryQmetric@users.noreply.github.com>
2019-03-04 13:13:24 +0000

    test for travis
    travis test

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -1,20 +1,20 @@
-import test from "ava";
-import {bitcoinPrice} from '../../src/index'
+//import test from "ava";
+//import {bitcoinPrice} from '../../src/index'
 
-test('throws an error if req.method is not POST', async t => {
-    const req = {
-        method: 'GET'
-    };
-    const res = ({
-        status: () => ({
-            send: () => {}
-        }) 
-    });
+//test('throws an error if req.method is not POST', async t => {
+    //const req = {
+       // method: 'GET'
+  //  };
+  //  const res = ({
+//     status: () => ({
+  //          send: () => {}
+    //    }) 
+    //});
 
-    const error = await t.throwsAsync(async () => await bitcoinPrice(req, res));
+ //   const error = await t.throwsAsync(async () => await bitcoinPrice(req, res));
 
-    t.is(error.code, 405);
-})
+   // t.is(error.code, 405);
+//})
 
 //test('ensure verify webhook is called', async t => {
     //const req = {
