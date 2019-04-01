0c7e60298cfbec8bed3860635199f1fcde66f053
HarryQmetric <46711154+HarryQmetric@users.noreply.github.com>
2019-04-01 12:57:00 +0100

    Deleted nock test

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -1,6 +1,5 @@
 import test from "ava";
 import {bitcoinPrice} from '../../src/index'
-import nock from 'nock';
 
 test('throws an error if req.method is not POST', async t => {
     const req = {
@@ -35,43 +34,3 @@ test('credentials are verified', async t => {
     t.is(error.code, 401);
 })
 
-test('calls api and returns the rate', async t => {
-    let actualResponse;
-
-    const req = {
-        method: 'POST',
-        body: {
-            token: '3FIjjMTBXUYXOQio6CYYxK3TN4bKE8XVAsBUGWbV-Rk=',
-            message: {
-                text: 'GBP'
-            }
-        }
-    };
-    const res = ({
-        status: () => ({
-            send: (response) => {
-                actualResponse = response;
-            }
-        }) 
-    });
-    
-    const server = nock('https://api.coindesk.com');
-
-    server.get('/v1/bpi/currentprice.json')
-        .reply(200, JSON.stringify({
-        bpi: {
-                'GBP': {
-                    rate: '666.45'
-                }
-            }
-    }));
-
-    const result = await bitcoinPrice(req, res);
-
-    t.is(actualResponse.cards[0].sections[0].widgets[0].textParagraph.text, '£666.45' )
-})
-
-const server = nock('https://api.coindesk.com');
-
-server.get('/v1/bpi/currentprice.json')
-    .replyWithError("invalid web path")
